3c64378aaf05c19fc1bd4c3f4b43b7630482218b
Dinesh Suthar <dineshds015@gmail.com>
2024-02-01 03:23:27 +0530

    React element and React components

--- App.js
+++ App.js
@@ -1,5 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const heading =React.createElement("h1",{id:"heading"},"Hello world from react!");
+
+//React Element
+const jsxHeading=<h1 id="heading" className="head" tabIndex="1">Namaste React using JSX🤖</h1>;
+
+//React Component
+
+const Title=()=>(
+    <h1 className="head" tabIndex="5">
+        Namaste React using Title
+    </h1>
+);
+
+const num=100;
+const HeadingComponent=()=>(
+    <div id="container">
+        {jsxHeading}
+        <Title/>
+        {
+            <h3>Hello from curly inside JSX {num}</h3>
+        }
+        <h1>Namaste React Functional Component</h1>
+    </div>
+    
+);
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+//render react element
+//root.render(jsxHeading);
+
+//render functional components
+root.render(<HeadingComponent/>);
